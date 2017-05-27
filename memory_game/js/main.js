f1b8d1a6b9efa487ff5a97818bfae4c16f0350f3
Geoff Dent <gldent@gmail.com>
2017-05-27 11:03:17 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,1 +1,20 @@
-console.log("Up and running!");
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("user flipped queen");
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("user flipped king");
+
+if (cardsInPlay.length === 2) {
+
+ 	if (cardsInPlay[0] === cardsInPlay[1]) {
+ 	alert("you found a match");
+	} else {
+ 	alert("Sorry, try again");
+	}
+}
+
+
