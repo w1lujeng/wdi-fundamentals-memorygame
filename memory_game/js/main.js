92671e16f43249d06bf7f31a87bd1f877e32f91d
Geoff Dent <gldent@gmail.com>
2017-05-27 13:17:12 -0700

    sightly corrected logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,10 +3,10 @@ var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 var cardOne = cards[0];
 cardsInPlay.push(cardOne);
-console.log("user flipped queen");
+console.log("user flipped " + cardOne);
 var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
-console.log("user flipped king");
+console.log("user flipped " + cardTwo);
 
 if (cardsInPlay.length === 2) {
 
@@ -14,6 +14,7 @@ if (cardsInPlay.length === 2) {
  	alert("you found a match");
 	} else {
  	alert("Sorry, try again");
+	
 	}
 }
 
